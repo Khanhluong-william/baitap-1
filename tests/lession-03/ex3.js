49d47f26a4423b72334eec64904603bcbb6b713f
Quốc Khánh <lgvkhanh@gmail.com>
2026-05-19 09:53:42 +0700

    init project

--- tests/lession-03/ex3.js
+++ tests/lession-03/ex3.js
@@ -4,7 +4,7 @@ for ( let i = 0; i < 100; i++) {
     if (i % 2 === 0) {
         console.log(`Sô ${i} là số chẵn`)
     } 
-    // nếu i chia hết cho 2 => Số lẻkhoong 
+    // nếu i chia hết cho 2 => Số lẻ 
     else {
         console.log(`Số ${i} là số lẻ`)
     }
